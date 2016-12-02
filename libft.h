ee83a77076c07b882c4eedef9f544dbfba2d3089
TMoska <tomas.moska@gmail.com>
2016-12-02 15:42:07 +0200

    no need to specify , pointers are not overlaping

--- libft.h
+++ libft.h
@@ -40,15 +40,15 @@ char				*ft_strnstr(const char *big, \
 char				*ft_strcpy(char *dst, const char *src);
 char				*ft_strncpy(char *dst, const char *src, size_t len);
 char				*ft_strdup(char *src);
-char				*ft_strcat(char *restrict s1, const char *restrict s2);
-char				*ft_strncat(char *restrict s1, \
-					const char *restrict s2, size_t n);
-size_t				ft_strlcat(char *restrict dst, const char *restrict src, \
+char				*ft_strcat(char *s1, const char *s2);
+char				*ft_strncat(char  *s1, \
+					const char *s2, size_t n);
+size_t				ft_strlcat(char *dst, const char *src, \
 					size_t size);
 void				*ft_memset(void *b, int c, size_t len);
 void				ft_bzero(void *s, size_t n);
-void				*ft_memcpy(void *restrict dst, \
-					const void *restrict src, size_t n);
+void				*ft_memcpy(void *dst, \
+					const void *src, size_t n);
 void				*ft_memmove(void *dst, const void *src, size_t len);
 int					ft_memcmp(const void *s1, const void *s2, size_t n);
 void				*ft_memalloc(size_t size);
@@ -74,8 +74,8 @@ void				ft_putchar_fd(char c, int fd);
 void				ft_putstr_fd(char const *s, int fd);
 void				ft_putendl_fd(char const *s, int fd);
 void				ft_putnbr_fd(int n, int fd);
-void				*ft_memccpy(void *restrict dst, \
-					const void *restrict src, int c, size_t n);
+void				*ft_memccpy(void *dst, \
+					const void *src, int c, size_t n);
 void				*ft_memchr(const void *s, int c, size_t n);
 char				*ft_strchr(const char *s, int c);
 char				*ft_strrchr(const char *s, int c);
